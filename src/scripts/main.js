82760dbd35530048a4f06c54bed90940e2030123
Evgeniy Mezhevykh <evgeniy.mezhevykh@ekreative.com>
2019-06-03 01:31:37 +0300

    last changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,13 @@ import './plugins/tabs'
 window.$ = window.jQuery = jQuery
 
 $('.features-tabs').tabs()
+
+let mobileMenu = $('.mobile-menu')
+
+$('button.mobile-icon').on('click', function () {
+  mobileMenu.addClass('active')
+})
+
+$('button.close-mobile-menu').on('click', function () {
+  mobileMenu.removeClass('active')
+})
